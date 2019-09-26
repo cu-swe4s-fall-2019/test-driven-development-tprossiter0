6777c09f14f16da0f38f145abc11a0557b8e760d
root <root@Unbound.localdomain>
2019-09-26 14:22:33 -0600

    test for list_mean(ints)

--- test_math_lib.py
+++ test_math_lib.py
@@ -1,7 +1,7 @@
 import random
 import math_lib
 import unittest
-import statistics
+import statistics as stat
 
 
 class TestMathLib(unittest.TestCase):
@@ -13,6 +13,11 @@ class TestMathLib(unittest.TestCase):
         emptylist = []
         ret = math_lib.list_mean(emptylist)
         self.assertEqual(ret, None)
+
+    def test_list_mean_ints(self):
+        intlist = [1, 2, 3, 4, 5, 6, -1, -5, -50]
+        ret = math_lib.list_mean(intlist)
+        self.assertEqual(ret, stat.mean(intlist))
 
 
 if __name__ == '__main__':
