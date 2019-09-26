58b4580b7f558189fcf5c50e37f2f0911d068660
root <root@Unbound.localdomain>
2019-09-26 15:47:42 -0600

    satisfies param tests, docstrings added

--- math_lib.py
+++ math_lib.py
@@ -5,6 +5,17 @@ import numpy as np
 
 
 def list_mean(L):
+    """ Compute arithmetic mean of a vector/list
+
+    Parameters
+    ----------
+    L - takes vector of integers or floats
+
+    Returns
+    ----------
+    avg - arithmetic mean of L
+
+    """
     if L is None:
         return None
     if len(L) == 0:
@@ -27,17 +38,32 @@ def list_mean(L):
 
 
 def list_stdev(L):
+
+    """ Compute standard deviation of a vector/list
+
+    Parameters
+    ----------
+    L - takes vector of integers or floats
+
+    Returns
+    ----------
+    stdev - standard deviation of the L
+
+    """
+
     if L is None:
         return None
     if len(L) == 0:
         return None
 
     try:
+        # exceptions with parameters caught below by ref to list_mean()
         mean = list_mean(L)
         rstdev = (sum([math.pow((mean-x), 2) for x in L]) / (len(L) - 1))
         rstdev = math.sqrt(rstdev)
         # needed to add n-1
     except Exception:
         raise Exception("Something went wrong with computation")
+        sys.exit(1)
 
     return rstdev
