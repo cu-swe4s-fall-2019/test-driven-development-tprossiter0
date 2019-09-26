5928d279008a04847ea53cf2c97ba02fff3016b0
root <root@Unbound.localdomain>
2019-09-26 15:42:07 -0600

    satisfies float, int test

--- math_lib.py
+++ math_lib.py
@@ -1,4 +1,7 @@
 import sys
+import math
+import statistics as stat
+import numpy as np
 
 
 def list_mean(L):
@@ -24,4 +27,17 @@ def list_mean(L):
 
 
 def list_stdev(L):
-    return None
+    if L is None:
+        return None
+    if len(L) == 0:
+        return None
+
+    try:
+        mean = list_mean(L)
+        rstdev = (sum([math.pow((mean-x), 2) for x in L]) / (len(L) - 1))
+        rstdev = math.sqrt(rstdev)
+        # needed to add n-1
+    except Exception:
+        raise Exception("Something went wrong with computation")
+
+    return rstdev
