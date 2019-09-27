d03ad1ac57f8a81b961ea12dc7375ec5624b816d
root <root@Unbound.localdomain>
2019-09-26 19:48:01 -0600

    tests each fn for already existing output file

---
+++ test_data_viz.py
@@ -0,0 +1,42 @@
+import unittest
+import random
+import statistics
+import data_viz
+import os.path
+from os import path
+
+
+class TestFileInputs(unittest.TestCase):
+    def test_boxplot_filename_already_exists(self):
+        test_filename = "test_file"
+        test_file = open(test_filename, "w")
+        test_file.close()
+
+        with self.assertRaises(Exception) as ex:
+            data_viz.boxplot([1, 2], test_filename)
+        output = str(ex.exception)
+        self.assertEqual(output, "error, file already exists")
+
+    def test_histogram_filename_already_exists(self):
+        test_filename = "test_file"
+        test_file = open(test_filename, "w")
+        test_file.close()
+
+        with self.assertRaises(Exception) as ex:
+            data_viz.histogram([1, 2], test_filename)
+        output = str(ex.exception)
+        self.assertEqual(output, "error, file already exists")
+
+    def test_combo_filename_already_exists(self):
+        test_filename = "test_file"
+        test_file = open(test_filename, "w")
+        test_file.close()
+
+        with self.assertRaises(Exception) as ex:
+            data_viz.combo([1, 2], test_filename)
+        output = str(ex.exception)
+        self.assertEqual(output, "error, file already exists")
+
+
+if __name__ == '__main__':
+    unittest.main()
