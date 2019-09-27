a1eec3bd93a952d75a4244ff36b83e941ba29569
root <root@Unbound.localdomain>
2019-09-26 19:48:29 -0600

    satisfies already existing output file tests

--- data_viz.py
+++ data_viz.py
@@ -1,9 +1,63 @@
+import math_lib as ml
+import matplotlib.pyplot as plt
+import os.path
+from os import path
+
 
 def boxplot(L, out_file_name):
+    """ Make Boxplot in an output file from the data in a numerical array
+
+    Parameters
+    ----------
+    L- Array of integers and/or floats to be plotted and analyzed
+    out_file_name - the name of the ouput file to which the plot,
+    mean and stdev will be given
+
+    Returns
+    ----------
+    No return value except for datastream to output file
+
+    """
+
+    if path.exists(out_file_name):
+        raise Exception("error, file already exists")
     pass
+
 
 def histogram(L, out_file_name):
+    """ Make Histogram in an output file from the data in a numerical array
+
+    Parameters
+    ----------
+    L- Array of integers and/or floats to be plotted and analyzed
+    out_file_name - the name of the ouput file to which the plot,
+    mean and stdev will be given
+
+    Returns
+    ----------
+    No return value except for datastream to output file
+
+    """
+    if path.exists(out_file_name):
+        raise Exception("error, file already exists")
     pass
 
+
 def combo(L, out_file_name):
+    """ Make Boxplot and Histogram in an output file from the
+    data in a numerical array
+
+    Parameters
+    ----------
+    L- Array of integers and/or floats to be plotted and analyzed
+    out_file_name - the name of the ouput file to which the plot,
+    mean and stdev will be given
+
+    Returns
+    ----------
+    No return value except for datastream to output file
+
+    """
+    if path.exists(out_file_name):
+        raise Exception("error, file already exists")
     pass
